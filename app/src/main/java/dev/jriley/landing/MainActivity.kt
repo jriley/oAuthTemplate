47763187a611e49ef7eeaae4dad39938c3d24e99
jriley <jasonriley5@gmail.com>
2018-12-30 15:37:28 -0600

    Added the activityies that are going to interact. jriley

---
+++ app/src/main/java/dev/jriley/landing/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.jriley.landing
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import dev.jriley.login.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
